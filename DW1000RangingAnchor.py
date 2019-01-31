0c79fdb9f9b95143bbe9449b2ca996118e0943ab
leo <742976921@qq.com>
2019-01-31 15:47:22 +0800

    Update DW1000RangingAnchor.py

--- DW1000RangingAnchor.py
+++ DW1000RangingAnchor.py
@@ -201,7 +201,7 @@ try:
     PIN_RST = 17   
     PIN_IRQ = 19
     PIN_SS = 27
-    # GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(PIN_RST, GPIO.IN)
     DW1000.begin(PIN_IRQ)
     DW1000.setup(PIN_SS)
@@ -214,7 +214,6 @@ try:
     DW1000.setAntennaDelay(C.ANTENNA_DELAY_RASPI)
 
     receiver()
-    transmitAnchorMsg(anchors)
     noteActivity()
     while 1:
         loop()
