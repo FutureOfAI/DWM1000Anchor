445ea6aaa50dc9bdf4d0dcc67464a9476e5ffbfd
leo <742976921@qq.com>
2019-01-31 15:43:52 +0800

    Update DW1000RangingAnchor.py

--- DW1000RangingAnchor.py
+++ DW1000RangingAnchor.py
@@ -147,15 +147,15 @@ def computeRangeAsymmetric():
 def loop():
     global sentAck, receivedAck, timePollAckSentTS, timePollReceivedTS, timePollSentTS, timePollAckReceivedTS, timeRangeReceivedTS, protocolFailed, data, expectedMsgId, timeRangeSentTS, \
         anchors, new_trans_flag
+    # if (sentAck == False and receivedAck == False):
+    #     if ((millis() - lastActivity) > C.RESET_PERIOD):
+    #         new_trans_flag = 1
+    #     return
 
     if new_trans_flag:
         new_trans_flag = 0
         transmitAnchorMsg(anchors)
         noteActivity()
-    if (sentAck == False and receivedAck == False):
-        if ((millis() - lastActivity) > C.RESET_PERIOD):
-            new_trans_flag = 1
-        return
 
     if sentAck:
         sentAck = False
